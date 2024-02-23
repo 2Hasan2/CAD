678a618191b291f0894f146d1ef07fb9724a86b8
2Hasan2 <h01554176846@gmail.com>
2024-02-24 01:02:10 +0200

    GUI

--- test.js
+++
@@ -1,5 +0,0 @@
-for (let zoomLevel = 1; zoomLevel <= 100; zoomLevel++) {
-    const scale = 5 * Math.pow(2, Math.round(Math.log2(25 / zoomLevel)));
-    console.log(`Zoom Level: ${zoomLevel}, Scale: ${scale}`);
-  }
-  
